69ded40a4a67d7e8cf96c2acb65f17e0a80bb0aa
DCarla <dcsouza119@gmail.com>
2023-09-11 16:43:39 -0300

    projetointegradorfrontupdates

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -2,9 +2,42 @@ import { StyledContainer } from "./style";
 import logo from "../../../public/assents/icons/logo.svg";
 import { goToCadastro, goToHomePrincipal } from "../../router/cordinator";
 import { Navigate, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { BASE_URL } from "../../constants/url";
+import axios from "axios";
+import { TOKEN_NAME } from "../../constants/url";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeForm = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+  const login = async (event) => {
+    event.preventDefault();
+    try {
+      setIsLoading(true);
+      const body = {
+        email: form.email,
+        password: form.password,
+      };
+
+      const response = await axios.post(`${BASE_URL}users/login`, body);
+      const authToken = response.data.token;
+
+      // Armazena o token no localStorage
+      window.localStorage.setItem(TOKEN_NAME, authToken);
+
+      setIsLoading(false);
+      goToHomePrincipal(navigate);
+    } catch (error) {}
+  };
+
   return (
     <StyledContainer>
       <div className="div1">
@@ -17,15 +50,31 @@ export default function Login() {
       </div>
 
       <div className="div2">
-        <input type="email" placeholder="E-mail" />
-        <input className="input2" type="password" placeholder="Senha" />
-
-        <button onClick={() => goToHomePrincipal(navigate)} className="button1">
-          Continuar
-        </button>
-        <hr />
-        <button onClick={() => goToCadastro(navigate)} className="button2">
-          Crie uma conta!
+        <form onSubmit={login} autoComplete="off">
+          <input
+            name="email"
+            value={form.email}
+            onChange={changeForm}
+            required
+            type="email"
+            placeholder="E-mail"
+          />
+          <input
+            name="password"
+            value={form.password}
+            onChange={changeForm}
+            required
+            type="password"
+            placeholder="Senha"
+            className="input2"
+          />
+          <button className="button1" onClick={login} disabled={isLoading}>
+            Continuar
+          </button>
+          <hr />
+        </form>
+        <button className="button2" onClick={() => goToCadastro(navigate)}>
+          Crie uma Conta!
         </button>
       </div>
     </StyledContainer>
